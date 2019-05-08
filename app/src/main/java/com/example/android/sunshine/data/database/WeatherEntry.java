5b0b2723d80736d7cfe601006f212f3445c1d4a3
Matthew Yang <19160743+m-yang@users.noreply.github.com>
2019-05-07 17:17:08 -0700

    add room annotations to WeatherEntry

--- app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
@@ -16,11 +16,18 @@
 
 package com.example.android.sunshine.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+@Entity(tableName = "weather", indices = {@Index(value = {"date"}, unique = true)})
 public class WeatherEntry {
 
-    private int id;
+    @PrimaryKey(autoGenerate = true)
+    public int id;
     private int weatherIconId;
     private Date date;
     private double min;
@@ -42,7 +49,20 @@ public class WeatherEntry {
      * @param wind Wind speed
      * @param degrees Wind direction
      */
+    @Ignore
     public WeatherEntry(int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.weatherIconId = weatherIconId;
+        this.date = date;
+        this.min = min;
+        this.max = max;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.wind = wind;
+        this.degrees = degrees;
+    }
+
+    public WeatherEntry(int id, int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.id = id;
         this.weatherIconId = weatherIconId;
         this.date = date;
         this.min = min;
