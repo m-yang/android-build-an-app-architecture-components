02fa5bb7747ba7389f95caae966d9bc3baa7293f
Matthew Yang <19160743+m-yang@users.noreply.github.com>
2019-05-08 14:24:38 -0700

    add database class

--- app/src/main/java/com/example/android/sunshine/data/database/WeatherDao.java
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherDao.java
@@ -1,11 +1,13 @@
 package com.example.android.sunshine.data.database;
 
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.Date;
 
+@Dao
 interface WeatherDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
